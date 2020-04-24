ad81e937b225a7d13cb8d82026d87ab8db44c034
Rebecca Biju <rebecca.biju@somaiya.edu>
2020-04-25 01:43:23 +0530

    Grid view displaying text from json data

--- flutter_blog_app/lib/HomePage.dart
+++ flutter_blog_app/lib/HomePage.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_blog_app/main.dart';
 
@@ -10,6 +12,21 @@ class HomePage extends StatefulWidget{
 class _HomeState extends State<HomePage>{
   @override
   Widget build(BuildContext context) {
+    // var myGridView = new GridView.builder(
+    //   itemCount: ,
+    //   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2), 
+    //   itemBuilder: (BuildContext context, int index){ //Have to pass something
+    //     return new GestureDetector(
+    //       child: new Card(
+    //         elevation: 5,
+    //         child: Container(
+    //           alignment: Alignment.center,
+    //           margin: EdgeInsets.only(top: 10, bottom: 10, left: 10),
+    //           child: Text("Hi")//Image has to be put here
+    //           )
+    //       )
+    //     );
+    //   });
     // TODO: implement build
     return new Scaffold(
 
@@ -27,7 +44,29 @@ class _HomeState extends State<HomePage>{
         ],)
       ),
 
-      body: Container(),
+      body: Center(
+        child: FutureBuilder(builder: (context, snapshot){
+          var showData = json.decode(snapshot.data.toString());
+          return GridView.builder(
+          itemCount: showData.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2), 
+          itemBuilder: (BuildContext context, int index){ //Have to pass something
+            return new GestureDetector(
+              child: new Card(
+                elevation: 5,
+                child: Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.only(top: 10, bottom: 10, left: 10),
+                  child: Text(showData[index]['msg'])//Image has to be put here
+                  )
+              )
+            );
+          });
+        }, future: DefaultAssetBundle.of(context).loadString("assets/sample.json"),
+        ),
+        ),
+      /* Code for a Button */
+      
       // floatingActionButton: Container(
       //   margin: EdgeInsets.only(left: 30),
       //   padding: EdgeInsets.symmetric(vertical: 15),
